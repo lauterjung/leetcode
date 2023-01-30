add5a26d7aa09f33e22461962a5a65aaf13fd03c
lauterjung <miguelbusarello@hotmail.com>
2023-01-30 13:03:00 -0300

    fizz-buzz 3

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -3,12 +3,12 @@ class Solution(object):
         answer = []
 
         for i in range(1, n+1):
-            result = ""
-
             if (i % 3) != 0 and (i % 5) != 0:
                 result = str(i)
                 answer.append(result)
                 continue
+            
+            result = ""
             if (i % 3) == 0:
                 result = result + "Fizz"
             if (i % 5) == 0:
