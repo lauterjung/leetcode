c1d079f33ea31d943ce23813e9ff316b3af955cc
lauterjung <miguelbusarello@hotmail.com>
2023-02-02 13:56:47 -0300

    WIP palindrome-linked-list 2

--- palindrome-linked-list/my-solution.py
+++ palindrome-linked-list/my-solution.py
@@ -1,24 +1,32 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         half_string: str = ""
-        count: int = 0
+        is_even: bool = False
+        is_odd: bool = True
+        count = 0
         slow = fast = head
         
         while slow:
             if (fast and fast.next):
                 half_string += str(slow.val)
-                fast = fast.next.next
-                print("a")
-                print(half_string)
+                fast = fast.next
+                count += 1
+
+                if fast.next: #fast.next.next
+                    fast = fast.next
+                    count += 1
+
+                else:
+                    half_string = half_string[:-1]
+                    is_even = True
+
             else:
-                print("b")
-                print(half_string[-1])
-                print(slow.val)
-                if half_string[-1] != slow.val:
+                # print("full: " + str(half_string))
+                # print("compared: " + str(half_string[-1]))
+                # print("actual: "+ str(slow.val))
+                if str(half_string[-1]) != str(slow.val):
                     return False
-                # half_string.pop()
-            
-            print("c")
-            print(slow.val)
+                half_string = half_string[:-1]
+
             slow = slow.next
         return True
